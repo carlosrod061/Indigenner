cea00c98199b3a9c1b573760b9f3ad2f8cc159c5
car121-stack <carlosrod061@gmail.com>
2021-03-10 13:57:06 -0600

    asd

---
+++ src/controllers/Novedades.js
@@ -0,0 +1,14 @@
+const controller = {};
+
+
+controller.list = (req, res) => {
+    res.render('novedades');
+};
+
+
+
+
+
+
+
+module.exports = controller;
--- src/routes/app_route.js
+++ src/routes/app_route.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const Inicio = require('../controllers/Inicio');
-
+const Novedades = require('../controllers/Novedades')
 
 router.get('/',Inicio.list);
+
+router.get('/novedades',Novedades.list);
 
 module.exports = router;
