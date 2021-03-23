76efcfbb1dd68c002d5f1f23e102502c8cde49e8
Jose_Gabriel_Gonzalez <pepexglzpard@outlook.com>
2021-03-23 12:52:50 -0600

    Update Foro.js

--- src/controllers/Foro.js
+++ src/controllers/Foro.js
@@ -7,11 +7,4 @@ controller.list = (req, res) => {
     res.render('foro',{usuario: req.session.user_id});
 };
 
-controller.foro = async (req, res) => {
-    const data = req.body;
-    const foros = db.collection('foro');
-    const foro = await foros.where('comentario', '==', true).get();
-    res.render('foro');
-};
-
 module.exports = controller;
