48212d8fb3a75eb4169e496db2547c9bbc9f642f
Jose_Gabriel_Gonzalez <pepexglzpard@outlook.com>
2021-03-23 12:52:54 -0600

    Update Foros.js

--- src/controllers/Foros.js
+++ src/controllers/Foros.js
@@ -3,15 +3,16 @@ const controller = {};
 const admin = require('firebase-admin');
 const db = admin.firestore();
 
-controller.list = (req, res) => {
-    res.render('foros',{usuario: req.session.user_id});
+controller.list = async (req, res) => {
+    
+    const juegos = db.collection('juego');
+    const juego = await juegos.get();
+    juego.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+    });
+    console.log(juego);
+    res.render('foros',{usuario: req.session.user_id, foroq: juego});
 };
 
-controller.foro = async (req, res) => {
-    const data = req.body;
-    const foros = db.collection('foro');
-    const foro = await foros.where('game', '==', 'Half Life 3').get();
-    res.render('foro');
-};
 
 module.exports = controller;
