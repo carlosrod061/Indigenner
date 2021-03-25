b057360c79d64f198e54d42aaa5b051fd7525f6d
Jose_Gabriel_Gonzalez <pepexglzpard@outlook.com>
2021-03-24 18:11:00 -0600

    Update Foro.js

--- src/controllers/Foro.js
+++ src/controllers/Foro.js
@@ -3,8 +3,10 @@ const controller = {};
 const admin = require('firebase-admin');
 const db = admin.firestore();
 
-controller.list = (req, res) => {
-    res.render('foro',{usuario: req.session.user_id});
+controller.list = async (req, res) => {
+    const comentarios = db.collection('foro');
+    const comentario = await comentarios.get();
+    res.render('foro',{usuario: req.session.user_id, foroq: comentario});
 };
 
 module.exports = controller;
