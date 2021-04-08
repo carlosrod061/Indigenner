939f71e6abd85eb48533366076cd096a0fc3b59c
Jose_Gabriel_Gonzalez <pepexglzpard@outlook.com>
2021-04-08 17:13:38 -0500

    Update Novedades.js

--- src/controllers/Novedades.js
+++ src/controllers/Novedades.js
@@ -1,8 +1,12 @@
 const controller = {};
 
+const admin = require('firebase-admin');
+const db = admin.firestore();
 
-controller.list = (req, res) => {
-    res.render('novedades',{usuario: req.session.user_id});
+controller.list = async (req, res) => {
+    const juegos = db.collection('juego');
+    const juego = await juegos.get();
+    res.render('novedades',{usuario: req.session.user_id, juegor: juego});
 };
 
 
