ff42cfeb3d4b49f12e5e6a9a7e4099e7ad57dd56
Jose_Gabriel_Gonzalez <pepexglzpard@outlook.com>
2021-04-15 20:37:23 -0500

    Update ModificarProyecto.js

--- src/controllers/ModificarProyecto.js
+++ src/controllers/ModificarProyecto.js
@@ -23,7 +23,24 @@ controller.abrirProyecto = async (req, res) => {
     res.render('modificarproyecto', {
         usuario: req.session.user_id, usur: usu, juegor: juego
     });
-
 }
 
+controller.actualizarproyecto = async(req,res) => {
+    const data = req.body;
+    const autores = db.collection('juego');
+    const autor = await autores.where('nombre', '==', data.nombre).get();
+
+    autor.forEach(doc => {
+        db.collection('juego').doc(doc.id).update({
+            descripcion:data.descripcion,
+            categoria:data.categoria
+        })  
+    });
+
+    const usuarios = db.collection('users');
+    const usu = await usuarios.where('user', '==', req.session.user_id).get();
+    const juegos = db.collection('juego');
+    const juego = await juegos.where('autor', '==', req.session.user_id).get();
+    res.render('perfil',{usuario: req.session.user_id, usur: usu, juegor: juego});
+}
 module.exports = controller;
